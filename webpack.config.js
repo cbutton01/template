ead5c6d79cbd9e8b70799358b3523d7043857762
Ami cooper and Cam Button <ami@>
2018-04-30 11:25:54 -0700

    fix dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -33,13 +33,21 @@ module.exports = {
         ]
       },
       {
-      test: /\.js$/,
-      exclude: [
-      /node_modules/,
-      /spec/
-    ],
-      loader: "eslint-loader"
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015']
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader'
+        }
+
+      ]
     }
-    ]
-  }
-};
+  };
